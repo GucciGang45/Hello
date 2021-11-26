9af7c15805986d477b3f276c38ed91b2437aff23
GucciGang45 <zouvictor25@gmail.com>
2021-11-26 15:39:47 +0800

    second commit

--- src/main/java/com/hzg/maven/Hello.java
+++ src/main/java/com/hzg/maven/Hello.java
@@ -1,6 +1,14 @@
 package com.hzg.maven;
 public class Hello {
-    public String sayHello(String name) {
+    public void sayHello(String name) {
         System.out.println("Hello " + name + "!");
+    }
+
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.sayHello("werwer");
+        hello.sayHello("werwer");
+        hello.sayHello("werwer");
+        hello.sayHello("werwer");
     }
 }
